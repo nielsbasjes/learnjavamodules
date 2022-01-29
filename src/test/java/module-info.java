8da6cbf940da5783f59b1186e513a4718437a7b5
Niels Basjes <niels@basjes.nl>
2022-01-29 16:26:53 +0100

    Show warnings

--- src/test/java/module-info.java
+++ src/test/java/module-info.java
@@ -3,8 +3,6 @@ open module tests.nl.basjes.jpms.experiment {
 
     requires com.esotericsoftware.kryo;
 
-    requires org.slf4j;
-    requires org.slf4j.simple;
     requires transitive org.junit.jupiter.engine;
     requires transitive org.junit.jupiter.api;
 }
